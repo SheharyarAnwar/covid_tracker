32e5230dfb58d4fc466e831e707a0811926d3a75
Sherry_XYZ <sheharyar28@hotmail.com>
2020-12-10 15:01:12 +0500

    chart responsivness checkpoint

--- src/Components/Plot/index.js
+++ src/Components/Plot/index.js
@@ -1,12 +1,16 @@
-import React, { createRef, useEffect } from "react";
+import React, { useState, createRef, useEffect } from "react";
 import Chart from "chart.js";
 import { useTheme } from "@material-ui/core";
-function Index() {
+function Index(props) {
+  const [canvasDimensions, setcanvasDimensions] = useState(props.dimensions);
   const ref = createRef();
   const theme = useTheme();
 
   useEffect(() => {
+    setcanvasDimensions(props.dimensions);
+    console.log(canvasDimensions);
     const canvasRef = ref.current;
+
     const globalSettings = Chart.defaults.global;
     Chart.defaults.scale.gridLines.drawOnChartArea = false;
     Chart.defaults.scale.ticks.fontColor = theme.palette.black;
@@ -14,13 +18,13 @@ function Index() {
     Chart.defaults.scale.gridLines.lineWidth = 2;
     globalSettings.legend.display = false;
     globalSettings.defaultFontFamily = theme.typography.fontFamily;
-
     const chart = new Chart(canvasRef.getContext("2d"), {
       type: "bar",
       data: {
         labels: ["Recovered", "Infected", "Deaths"],
         datasets: [
           {
+            barPercentage: 0.4,
             data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
               theme.palette.darkBlue,
@@ -31,25 +35,26 @@ function Index() {
         ],
       },
       options: {
+        maintainAspectRatio: false,
         scales: {
-          xAxes: [
-            {
-              barPercentage: 0.4,
-            },
-          ],
           yAxes: [
             {
-              barPercentage: 0.4,
               ticks: { padding: 10 },
             },
           ],
         },
       },
     });
-  }, []);
+    chart.canvas.parentNode.style.height = canvasDimensions.height;
+    chart.canvas.parentNode.style.width = canvasDimensions.width;
+  });
   return (
     <>
-      <canvas width={320} height={280} ref={ref}></canvas>
+      <canvas
+        height={canvasDimensions.height}
+        width={canvasDimensions.width}
+        ref={ref}
+      ></canvas>
     </>
   );
 }
--- src/Views/Main/index.js
+++ src/Views/Main/index.js
@@ -1,5 +1,5 @@
-//import { useEffect } from "react";
-import { Hidden, Grid, useMediaQuery, useTheme } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import { Hidden, Grid, useMediaQuery } from "@material-ui/core";
 import Sidebar from "../../Components/Sidebar/index";
 import Header from "../../Components/Header/index";
 import Dropdown from "../../Components/Dropdown/index";
@@ -7,20 +7,23 @@ import CardList from "../../Components/Card/CardList/index";
 import Plot from "../../Components/Plot/index";
 import style from "./styles";
 function Index() {
+  const [dimensions, setdimensions] = useState({ width: 320, height: 280 });
   const mdMatch = useMediaQuery((theme) => theme.breakpoints.up("md"));
   const smMatch = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   const brMatch = useMediaQuery((theme) =>
     theme.breakpoints.between("xs", window.screen.width * 0.48)
   );
-  const theme = useTheme();
   const match = { mdMatch, smMatch, brMatch };
-  /*  window.addEventListener("resize", () => {
-    console.log("brMatch:", brMatch, "smMatch:", smMatch, "mdMatch:", mdMatch);
-  }); */
-  console.log(window.screen.width * 0.48);
-  let marginOffset = mdMatch ? { marginBottom: "5%" } : { marginBottom: "5%" };
+
+  useEffect(() => {
+    if (brMatch) {
+      setdimensions({ width: 280, height: 255 });
+    } else {
+      setdimensions({ width: 320, height: 280 });
+    }
+  }, [brMatch]);
   let styles = style(match);
-  let headerWidth = smMatch ? 12 : 0;
+  let headerWidth = smMatch ? 12 : false;
   return (
     <Grid container>
       <Grid item xs={2}>
@@ -43,14 +46,14 @@ function Index() {
         xs={12}
         md={10}
       >
-        <Grid item container justify="center" style={marginOffset}>
+        <Grid item container justify="center" style={{ marginBottom: "3%" }}>
           <Grid item xs={4} sm={2}>
             <Dropdown />
           </Grid>
         </Grid>
-        <CardList marginOffset={marginOffset} />
-        <Grid item style={{ margin: brMatch ? "8%" : null }}>
-          <Plot />
+        <CardList marginOffset={{ marginBottom: "3%" }} />
+        <Grid item>
+          <Plot dimensions={dimensions} />
         </Grid>
       </Grid>
     </Grid>
